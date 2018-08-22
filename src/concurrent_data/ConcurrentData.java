12870be4ac6c27beb3f5ac69e3e3b21d13696688
LzGustavo <xavier.luizgustavo@gmail.com>
2018-08-22 14:36:22 -0300

    modified log archive structure

--- src/concurrent_data/ConcurrentData.java
+++ src/concurrent_data/ConcurrentData.java
@@ -118,37 +118,62 @@ public class ConcurrentData {
 				System.out.println(key + " " + data.get(key));
 			}
 
-			for (int i = 0; i < workers.size(); i++) {
-				workers.get(i).getStatus();
+			Path file_remove = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-remove.txt");
+			Path file_read = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-read.txt");
+			Path file_write = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-write.txt");
+			
+			try {
+				
+				// create the empty file with default permissions, etc.
+				Files.createFile(file_remove);
+				Files.createFile(file_read);
+				Files.createFile(file_write);
+				
+				for (int i = 0; i < workers.size(); i++) {
+					workers.get(i).getStatus();
 
-				Path file = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-worker"+i+".txt");
-				try {
-					// Create the empty file with default permissions, etc.
-					Files.createFile(file);
-					
 					ArrayList log_ops = workers.get(i).getLog();
 					ArrayList latency = workers.get(i).getLatency();
-					
-					String aux_buff = new String();
-					
+
+					String aux_remove = new String();
+					String aux_read = new String();
+					String aux_write = new String();
+
 					for (int j = 0; j < log_ops.size(); j++) {
-						
-						aux_buff += log_ops.get(j) + " - " + latency.get(j) + "\n";
+
+						if (log_ops.get(j).toString().contains("Remove"))
+							aux_remove += "worker" + i + " - " + log_ops.get(j) + " - " + latency.get(j) + "\n";
+						else if (log_ops.get(j).toString().contains("Read"))
+							aux_read += "worker" + i + " - " + log_ops.get(j) + " - " + latency.get(j) + "\n";
+						else
+							aux_write += "worker" + i + " - " + log_ops.get(j) + " - " + latency.get(j) + "\n";
 					}
 					
-					byte[] buffer = aux_buff.getBytes();
-					Files.write(file, buffer);
+					Files.write(file_remove, aux_remove.getBytes(), StandardOpenOption.APPEND);
+					Files.write(file_read, aux_read.getBytes(), StandardOpenOption.APPEND);
+					Files.write(file_write, aux_write.getBytes(), StandardOpenOption.APPEND);
 				}
-				catch (FileAlreadyExistsException x) {
-					System.err.format("file named %s" +" already exists %s%n", file, x);
-				}
-				catch (IOException x) {	
-					System.err.format("createFile error: %s%n", x);
-				}
+			}
+			catch (FileAlreadyExistsException x) {
+				System.err.format("file named already exists %s%n", x);
+			}
+			catch (IOException x) {	
+				System.err.format("createFile error: %s%n", x);
+			}
+			catch (Exception y) {
+				System.err.format("Exception encountered: %s%n", y);
 			}
 			
 			//all workers operate on the same data structure reference
 			workers.get(0).Show();
+			
+			//lock execution till any key input
+			Scanner scan = new Scanner(System.in);
+			String msg;
+			do {
+				msg = scan.nextLine();
+			} while (!msg.equals(""));
+			
 		}
 	}
 }
