3e9bbd7ac845519e4b6af27d9c30a3949b0f0b27
LzGustavo <xavier.luizgustavo@gmail.com>
2018-08-24 11:09:39 -0300

    minor code changes and started a GUI

--- src/concurrent_data/ConcurrentData.java
+++ src/concurrent_data/ConcurrentData.java
@@ -18,6 +18,31 @@ public class ConcurrentData {
 	//choosen from config param.
 	private static Object DataStruct;
 	
+	/*public ConcurrentData() {
+		// RAII
+		
+		try {
+			File config_file = new File("/home/lzgustavo/NetBeansProjects/concurrent-data/test/config.txt");
+			
+			file_remove = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-remove.txt");
+			file_read = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-read.txt");
+			file_write = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-write.txt");
+
+			Files.createFile(file_remove);
+			Files.createFile(file_read);
+			Files.createFile(file_write);
+		}
+		catch (FileAlreadyExistsException x) {
+			System.err.format("file named already exists %s%n", x);
+		}
+		catch (IOException x) {	
+			System.err.format("createFile error: %s%n", x);
+		}
+		catch (Exception e) {
+			System.err.format("Exception caught: %s%n", e);
+		}
+	}*/
+	
 	public static HashMap ConfigParam(File config) {
 		// extract info from config file
 		HashMap info = new HashMap();
@@ -30,7 +55,7 @@ public class ConcurrentData {
 			}
 		}
 		catch (FileNotFoundException e) {
-			System.out.println("Exception: " +e);
+			System.err.format("Exception: %s%n", e);
 		}
 		
 		return info;
@@ -99,7 +124,7 @@ public class ConcurrentData {
 			}
 		}
 		catch (InterruptedException e) {
-			System.out.println("Exception: " +e);
+			System.err.format("Exception: %s%n", e);
 		}
 	}
 	
@@ -117,63 +142,64 @@ public class ConcurrentData {
 			for (Object key : data.keySet()) {
 				System.out.println(key + " " + data.get(key));
 			}
-
-			Path file_remove = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-remove.txt");
-			Path file_read = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-read.txt");
-			Path file_write = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-write.txt");
-			
-			try {
-				
-				// create the empty file with default permissions, etc.
-				Files.createFile(file_remove);
-				Files.createFile(file_read);
-				Files.createFile(file_write);
-				
-				for (int i = 0; i < workers.size(); i++) {
-					workers.get(i).getStatus();
-
-					ArrayList log_ops = workers.get(i).getLog();
-					ArrayList latency = workers.get(i).getLatency();
-
-					String aux_remove = new String();
-					String aux_read = new String();
-					String aux_write = new String();
-
-					for (int j = 0; j < log_ops.size(); j++) {
-
-						if (log_ops.get(j).toString().contains("Remove"))
-							aux_remove += "worker" + i + " - " + log_ops.get(j) + " - " + latency.get(j) + "\n";
-						else if (log_ops.get(j).toString().contains("Read"))
-							aux_read += "worker" + i + " - " + log_ops.get(j) + " - " + latency.get(j) + "\n";
-						else
-							aux_write += "worker" + i + " - " + log_ops.get(j) + " - " + latency.get(j) + "\n";
-					}
-					
-					Files.write(file_remove, aux_remove.getBytes(), StandardOpenOption.APPEND);
-					Files.write(file_read, aux_read.getBytes(), StandardOpenOption.APPEND);
-					Files.write(file_write, aux_write.getBytes(), StandardOpenOption.APPEND);
-				}
-			}
-			catch (FileAlreadyExistsException x) {
-				System.err.format("file named already exists %s%n", x);
-			}
-			catch (IOException x) {	
-				System.err.format("createFile error: %s%n", x);
-			}
-			catch (Exception y) {
-				System.err.format("Exception encountered: %s%n", y);
-			}
-			
-			//all workers operate on the same data structure reference
-			workers.get(0).Show();
-			
-			//lock execution till any key input
-			Scanner scan = new Scanner(System.in);
-			String msg;
-			do {
-				msg = scan.nextLine();
-			} while (!msg.equals(""));
-			
 		}
+
+		Path file_remove = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-remove.txt");
+		Path file_read = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-read.txt");
+		Path file_write = Paths.get("/home/lzgustavo/NetBeansProjects/concurrent-data/test/log-write.txt");
+
+		try {
+
+			// create the empty file with default permissions, etc.
+			Files.createFile(file_remove);
+			Files.createFile(file_read);
+			Files.createFile(file_write);
+
+			for (int i = 0; i < workers.size(); i++) {
+				workers.get(i).getStatus();
+
+				ArrayList log_ops = workers.get(i).getLog();
+				ArrayList latency = workers.get(i).getLatency();
+
+				String aux_remove = new String();
+				String aux_read = new String();
+				String aux_write = new String();
+
+				for (int j = 0; j < log_ops.size(); j++) {
+
+					if (log_ops.get(j).toString().contains("Remove"))
+						aux_remove += "worker" + i + " - " + log_ops.get(j) + " - " + latency.get(j) + "\n";
+					else if (log_ops.get(j).toString().contains("Read"))
+						aux_read += "worker" + i + " - " + log_ops.get(j) + " - " + latency.get(j) + "\n";
+					else
+						aux_write += "worker" + i + " - " + log_ops.get(j) + " - " + latency.get(j) + "\n";
+				}
+
+				Files.write(file_remove, aux_remove.getBytes(), StandardOpenOption.APPEND);
+				Files.write(file_read, aux_read.getBytes(), StandardOpenOption.APPEND);
+				Files.write(file_write, aux_write.getBytes(), StandardOpenOption.APPEND);
+			}
+		}
+		catch (FileAlreadyExistsException x) {
+			System.err.format("file named already exists %s%n", x);
+		}
+		catch (IOException x) {	
+			System.err.format("createFile error: %s%n", x);
+		}
+		catch (Exception y) {
+			System.err.format("Exception encountered: %s%n", y);
+		}
+
+		//all workers operate on the same data structure reference
+		workers.get(0).Show();
+
+		//lock execution till any key input
+		Scanner scan = new Scanner(System.in);
+		String msg;
+		do {
+			msg = scan.nextLine();
+		} while (!msg.equals(""));
+			
+		
 	}
 }
